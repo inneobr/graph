8d191bbd3877a1eee7de6b914dbf4fd095b76413
cloundfile <tecnologiaesolucoespr@gmail.com>
2025-10-21 20:03:33 -0300

    ajuste graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Previsão do Tempo - Aplicativo Climático",
-  description: "Acompanhe as condições climáticas em tempo real com previsões precisas do tempo para hoje.",
+  title: "Graph",
+  description: "Apis inneo.org",
 };
 
 export default function RootLayout({
@@ -25,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
